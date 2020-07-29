b6fa5c5c251906854f7e4f483f2316e4f5ff136e
Katrina Wheelan <kw8@williams.edu>
2020-07-29 12:14:14 -0400

    typo

--- pythonScripts/regress.py
+++ pythonScripts/regress.py
@@ -86,12 +86,18 @@ Clean up and prep the data for analysis.
 
 
 #standardize data, trim dates, add month and constant cols
+
+print(f"mslp: mean: {np.mean(X_all.mslp)} var: {np.var(X_all.mslp)}\n")
+
 if stdize:
     predictors = standardize(predictors)
 X_all, Y_all = prep_data(obsPath, predictors, lat, lon, dateStart = dateStart, dateEnd = dateEnd)
 X_all, Y_all, all_preds = add_month(X_all, Y_all)
 X_all, all_preds = add_constant_col(X_all)
 print("Loaded obs data.")
+
+print(f"predictors mslp: mean: {np.mean(predictors.mslp)} var: {np.var(predictors.mslp)}\n")
+print(f"X_all stdized mslp: mean: {np.mean(X_all.mslp)} var: {np.var(X_all.mslp)}\n")
 
 if train:
     #separate testing and training data by even and odd years
