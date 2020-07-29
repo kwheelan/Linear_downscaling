f33f8847a00b0de1fe38cb0d1e51a36c0762ab04
Katrina Wheelan <kw8@williams.edu>
2020-07-29 10:00:40 -0400

    typo

--- pythonScripts/plotting.py
+++ pythonScripts/plotting.py
@@ -186,12 +186,12 @@ def save_stats(Y_all, preds, lat, lon, save_path):
                lon - longitude as a float
         Output: None
         # TODO: r-squared; standard error
-    """ 
+    """
     plot_path = make_plot_folder(save_path, lat, lon)
     f = open(os.path.join(plot_path, "stats.txt"), "w")
     f.write("Observations:")
-    f.write(f"Mean: {float(mean(Y_all.tmax))}")
-    f.write(f"Variance: {float(var(Y_all.tmax))}\n")
+    f.write(f"Mean: {float(np.mean(Y_all.tmax))}")
+    f.write(f"Variance: {float(np.var(Y_all.tmax))}\n")
     f.write("Modeled Data:")
     f.write(f"Mean: {float(mean(preds.preds))}")
     f.write(f"Variance: {float(var(preds.preds))}\n")
