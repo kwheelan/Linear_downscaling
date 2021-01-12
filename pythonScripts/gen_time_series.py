5f3d8fb996336501e9efdd54ea3ad2b6e5e75aa3
Katrina Wheelan <kw8@williams.edu>
2021-01-12 11:48:18 -0500

    changes

--- pythonScripts/gen_time_series.py
+++ pythonScripts/gen_time_series.py
@@ -89,7 +89,7 @@ if settings['stdize']:
     #standardize predictors
     if settings['monthly']:
         #standardize data by month
-        path = f"{ROOT}/anom.txt".replace('rcp85', 'historical')
+        path = f"{ROOT}/anoms.txt".replace('rcp85', 'historical')
         if not future:
             X_all = stdz_month(X_all, anomSavePath = path)
         else:
