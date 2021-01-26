494d2590ea244dde1985f9ccfff21d57f014f736
Katrina Wheelan <kw8@williams.edu>
2021-01-26 12:17:41 -0500

    1.26

--- pythonScripts/gen_time_series.py
+++ pythonScripts/gen_time_series.py
@@ -158,7 +158,7 @@ if not future:
 else:
     #future projections
     #set years equal to plot
-    final_predictions['time'] = int(final_predictions.time[0]) - int(Y_all.time[0]) + final_predictions.time
+    final_predictions['time'] =  int(Y_all.time[0]) - int(final_predictions.time[0]) + final_predictions.time
     print(final_predictions)
     plotData = Plot(save_location, lat, lon, predictand, obs = Y_all,
                 models = {'downscaled GCM': final_predictions.sel(time=slice('1980-01-01', '2005-12-31'))},
